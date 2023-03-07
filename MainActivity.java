377995f59f7ce560c954b3e17938d2a540e9fd2c
sriman001 <110296786+sriman001@users.noreply.github.com>
2023-03-07 05:57:28 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.tutlane.loginexample;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView register = (TextView)findViewById(R.id.lnkRegister);
+        register.setMovementMethod(LinkMovementMethod.getInstance());
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
